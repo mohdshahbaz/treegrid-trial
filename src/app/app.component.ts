7fc512ce664ac9d5bfe79acaa111cb5db4bd6a0f
shahbaz <mohdshahbaz@gmail.com>
2021-09-27 12:54:07 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { EditSettingsModel } from '@syncfusion/ej2-angular-treegrid';
+import { sampleData } from './datasource';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  public data: Object[] = [];
+  public contextMenuItems: Object[] = [];
+  public editSettings: EditSettingsModel = {};
+  public filterSettings: Object = {};
+  public selectionSettings: Object = {};
+
   title = 'treegrid-trial';
+
+  ngOnInit() {
+    this.data = sampleData;
+
+    this.editSettings = {
+      allowEditing: true,
+      allowAdding: true,
+      allowDeleting: true,
+      mode: 'Cell',
+    };
+
+    this.selectionSettings = { type: 'Multiple' };
+
+    this.filterSettings = { type: 'Menu' };
+
+    this.contextMenuItems = [
+      'Data Type',
+      'Default Value',
+      'Font',
+      'Color',
+      'Alignment',
+      'Text-wrap',
+      'AutoFit',
+      'AutoFitAll',
+      'SortAscending',
+      'SortDescending',
+      'Edit',
+      'Delete',
+      'Save',
+      'Cancel',
+      'PdfExport',
+      'ExcelExport',
+      'CsvExport',
+    ];
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {
+  ColumnMenu,
+  ColumnMenuService,
+  ContextMenuService,
+  EditService,
+  ExcelExportService,
+  FilterService,
+  PdfExportService,
+  ReorderService,
+  ResizeService,
+  SortService,
+  TreeGridModule,
+} from '@syncfusion/ej2-angular-treegrid';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [AppComponent],
+  imports: [BrowserModule, TreeGridModule],
+  providers: [
+    ResizeService,
+    ContextMenuService,
+    SortService,
+    ExcelExportService,
+    PdfExportService,
+    EditService,
+    ReorderService,
+    ColumnMenuService,
+    FilterService,
   ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
